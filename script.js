d9d5a4220af15f1856e527173cec01cf83a0ad30
jorgefpaz224 <jorgefpaz224@gmail.com>
2025-06-08 18:23:49 -0600

    Added SpeechSynthesis API & Modified speech lang n voice

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let texto = "";
 
 function guardarConversacion() {
   const mensajes = document.querySelectorAll(".message");
-  mensajes.forEach(msg => {
+  mensajes.forEach((msg) => {
     texto += msg.textContent + "\n";
   });
 
@@ -15,7 +15,7 @@ function guardarConversacion() {
 
 function limpiarConversacion() {
   const mensajes = document.querySelectorAll(".message");
-  mensajes.forEach(msg => msg.remove());
+  mensajes.forEach((msg) => msg.remove());
 
   console.log("Conversación limpiada.");
   alert("✅ Conversación limpiada correctamente");
@@ -30,7 +30,7 @@ function mostrarConversaciones() {
 }
 
 function enviarMensaje(event) {
-  event.preventDefault(); 
+  event.preventDefault();
 
   const input = document.getElementById("messageInput");
   const mensaje = input.value.trim();
@@ -47,4 +47,36 @@ function enviarMensaje(event) {
   messageBox.appendChild(nuevoMensaje);
 
   input.value = ""; // limpiar campo de texto
+}
+
+
+function leerMensajes() {
+  const mensajes = document.querySelectorAll(".message");
+
+  if (mensajes.length === 0) {
+    alert("No hay mensajes para leer.");
+    return;
+  }
+
+  let texto = "";
+  mensajes.forEach((msg) => {
+    texto += msg.textContent + ". ";
+  });
+
+  const utterance = new SpeechSynthesisUtterance(texto);
+  utterance.lang = "es_US"; 
+  utterance.rate = 1; // velocidad de lectura (1 es normal)
+
+  const voices = window.speechSynthesis.getVoices();
+
+  //Se instancia la voz femenina soportada por Google
+  const femenineVoice = voices.find(voice => voice.name === "Google español de Estados Unidos");
+  if (femenineVoice) {
+    utterance.voice = femenineVoice;
+  } else {
+    console.warn("No se encontró la voz femenina preferida.");
+  }
+  window.speechSynthesis.speak(utterance);
+
+  speechSynthesis.getVoices().forEach((voz, i) => console.log(i, voz.name, voz.lang));
 }
